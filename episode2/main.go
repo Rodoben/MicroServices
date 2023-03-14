2f3bd1aa77d7bb6ccd7226273c2648a08df206cd
Ronald Benjamin <dodo.ben.rb@gmail.com>
2023-03-14 11:48:36 +0530

    updated main

--- episode2/main.go
+++ episode2/main.go
@@ -1,25 +1,27 @@
 package main
 
 import (
+	"context"
 	"episode2/handler"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
 	fmt.Println("server started")
 
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
-	fmt.Println("l:", l)
+
 	hh := handler.NewHello(l)
 	gg := handler.NewGoodBye(l)
-	fmt.Println("hh:", hh)
 	ww := handler.NewWelcome(l)
+
 	sm := http.NewServeMux()
 
-	fmt.Println("sm:", sm)
 	sm.Handle("/", hh)
 	sm.Handle("/bye", gg)
 	sm.Handle("/welcome", ww)
@@ -28,6 +30,35 @@ func main() {
 		log.Println("hi log")
 	})
 
-	http.ListenAndServe(":9095", sm)
+	s := &http.Server{
+		Addr:         ":9095",
+		Handler:      sm,
+		WriteTimeout: 1 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		IdleTimeout:  120 * time.Second,
+	}
+
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+
+	}()
+
+	// trap sigterm or interupt and gracefully shutdown the server
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	signal.Notify(c, os.Kill)
+
+	// Block until a signal is received.
+	sig := <-c
+	log.Println("Got signal:", sig)
+
+	// gracefully shutdown the server, waiting max 30 seconds for current operations to complete
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(ctx)
+
+	//http.ListenAndServe(":9095", sm)
 
 }
